680dfe8fb0edca12408c29f26bcc94e04748dc08
Gregory Cheng <gregorycheng@Gregorys-MacBook-Pro.local>
2018-11-28 16:43:45 +1300

    Added create survey functionality, lists surveys on home page

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,4 +1,21 @@
 class SurveysController < ApplicationController
   def index
+    @surveys = SurveyPost.all
+  end
+
+  def new
+  end
+
+  def create
+    @survey = SurveyPost.new(survey_params)
+    @survey.save
+
+    redirect_to surveys_path
+  end
+
+  private
+
+  def survey_params
+    params.require(:survey_post).permit(:title, :body)
   end
 end
