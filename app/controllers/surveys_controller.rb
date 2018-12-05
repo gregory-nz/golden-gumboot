d42debbec64b8912035e7d8be631923ce2f229eb
Gregory Cheng <gregorycheng@Gregorys-MacBook-Pro.local>
2018-12-06 11:03:18 +1300

    Added Edit survey functionality + +edit buttons + CSS changes

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,28 +1,42 @@
 class SurveysController < ApplicationController
   before_action :login_redirect
+  before_action :find_survey, only: [:show, :edit, :update, :destroy]
   def index
     @surveys = current_user.surveys
   end
 
   def new
+    @survey = Survey.new
   end
 
   def create
     @survey = current_user.surveys.new(survey_params)
     if @survey.save
-      redirect_to surveys_path
+      redirect_to root_path
     else
       render 'new'
     end
   end
 
   def show
-    @survey = Survey.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @survey.update(survey_params)
+      flash[:success] = "Survey successfully updated!"
+      redirect_to @survey
+    else
+      flash[:danger] = "Failed to edit form!"
+      render 'edit'
+    end
   end
 
   def destroy
-    @survey = Survey.find(params[:id])
     @survey.destroy
+    flash[:danger] = @survey.title + " successfully deleted"
     redirect_to root_path
   end
 
@@ -34,5 +48,9 @@ class SurveysController < ApplicationController
 
   def login_redirect
     redirect_to login_path unless logged_in?
+  end
+
+  def find_survey
+    @survey = Survey.find(params[:id])
   end
 end
